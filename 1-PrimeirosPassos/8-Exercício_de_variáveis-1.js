c31262eb6260925f0d52ccd5a87a2cc1285a6de3
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-24 20:39:35 -0300

    Criado 8-Exercício_de_variável-1.js

---
+++ 1-PrimeirosPassos/8-Exercício_de_variáveis-1.js
@@ -0,0 +1,13 @@
+//? 1. Crie uma variável para armazenar o nome
+//? do carro e ponha o valor Ferrari. E exiba-o.
+var carro = "Ferrari"
+console.log(carro)
+
+//? 2. Crie um avariável para armazenar o preço 
+//? do bolo, que custa R$ 19,99. E exiba-a.
+var preco = 19.99
+console.log(preco)
+
+//? Corrija o código a seguir (e exiba-o).
+let cidade = "São Paulo"
+console.log(cidade)
