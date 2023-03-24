e62039a4098235f7a4e922e28e8adcf3c3645701
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-24 20:57:06 -0300

    Aquivo 10-let_const_var.js criado

---
+++ 1-PrimeirosPassos/10-let_const_var.js
@@ -0,0 +1,20 @@
+let nome = "Ederson Programador"
+console.log(nome)
+nome = "João"
+console.log(nome)
+
+// ^ Qual eu crio uma variável com valor let é possível alterar o valor ^
+
+var nome2 = "Ederson Programador"
+console.log(nome2)
+
+nome2 = "João2"
+console.log(nome2)
+
+//? O let e o var funcionam exatamente igual para criação de variáveis.
+
+
+// ^ Algo que é constante é algo que não para é algo que não muda.
+
+const nome3 = "Ederson Programador Fullstack"
+console.log(nome3)
