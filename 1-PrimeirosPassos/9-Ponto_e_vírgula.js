09513af899376573b6bb923284e949936c44537b
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-24 20:44:54 -0300

    Adicinado o aquivo 9-Ponto_e_vírlula

---
+++ 1-PrimeirosPassos/9-Ponto_e_vírgula.js
@@ -0,0 +1,8 @@
+let nome = "Bonieky"; let idade = 90;
+
+let nome2 = "Ederson Programador"
+let idade2 = 90
+
+//^ Quanto tiver vários códigos em uma mesma linha o ponto e vígula se torna obrigatório.
+
+console.log(nome); console.log(idade);
