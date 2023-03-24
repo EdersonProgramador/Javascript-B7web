bd9106022c234ec8e43b9367d8e4378098b03241
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-24 20:25:45 -0300

    7-Comentário_no_código

---
+++ 1-PrimeirosPassos/7-Comentário_no_código.js
@@ -0,0 +1,13 @@
+let nome = "Ederson" // string
+let idade = 90 // number
+let logado = true // boolean
+let lista = ["ovo", "massa", "corante"] // array
+let nomeCompleto = { nome: "Ederson", idade: 90 } // object
+
+// o comando abaixo exibe os dados no CONSOLE
+console.log()
+console.log(typeof nomeCompleto)
+
+/**
+ *? Comentário de multiplas linhas
+ */
