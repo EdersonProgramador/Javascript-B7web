370f945fd024944507754447c6a3ddb70ed9ef22
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-24 15:15:53 -0300

    5-variaveis.js, Finalizado

---
+++ 1-PrimeirosPassos/5-variaveis.js
@@ -0,0 +1,11 @@
+// Valor que armazena informações
+
+x = 10;
+
+let nome = "Ederson,";
+let idade = 90;
+let endereco = "Pleno Residencial - Ananindeua-PA,"
+let cep = "68145-000"
+
+
+console.log(nome, idade, endereco, cep)
