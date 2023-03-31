7e9f7e6fe76d68f1def0a9aa4132c1bc268bd82e
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-31 12:02:29 -0300

    Iniciando 12. Condicional if/else

---
+++ 1-PrimeirosPassos/11.String_Template_String.js
@@ -0,0 +1,7 @@
+let nome = "Ederson";
+let sobrenome = "Programador";
+
+// let nomaCompleto = nome + ' ' + sobrenome;
+let nomeCompleto = `${nome} ${sobrenome} FullStack`;
+
+console.log("Nome: " + nomeCompleto);
