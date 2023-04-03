8ae7bf83a88cf8f127cf80932d2197c61013e1b9
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-04-03 16:41:06 -0300

    retornando funçao

---
+++ 2-JavascriptBasico/5.retorno_da_funcao.js
@@ -0,0 +1,8 @@
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`;
+}
+
+let completo = nomeCompleto("Ederson", "Programador");
+console.log('Fullstack: ' + completo)
+
+// ENTRADA -> PROCESSAMENTO -> SAIDA
