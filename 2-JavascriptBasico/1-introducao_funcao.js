ace675ef172a5b7fa99db1e868887eac36860390
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-04-03 16:08:27 -0300

    Declarando uma função

---
+++ 2-JavascriptBasico/1-introducao_funcao.js
@@ -0,0 +1,5 @@
+// Uma função é uma sequência de instruções que executa uma tarefa específica e pode ser reutilizada em diferentes partes de um programa. Em programação, as funções são usadas para dividir um programa em partes menores e mais gerenciáveis, permitindo que o código seja organizado de forma mais clara e modular.
+
+// As funções geralmente recebem entradas (chamadas de argumentos ou parâmetros) e produzem saídas (chamadas de retorno). Elas podem executar cálculos, manipular dados, exibir informações para o usuário e realizar outras tarefas.
+
+// As funções são amplamente utilizadas em várias linguagens de programação, incluindo Python, Java, C++, JavaScript, entre outras. Elas são uma parte fundamental da programação e ajudam os desenvolvedores a escreverem códigos mais eficientes e legíveis.
