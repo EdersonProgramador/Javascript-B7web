c120d922b93fbaed0bf052cf2355b1e6181b5835
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-12 18:55:15 -0300

    Arrow Function

---
+++ 2-JavascriptBasico/10-Arrow_Function.js
@@ -0,0 +1,16 @@
+// function somar(x, y) {
+//     return x + y;
+// }
+
+// const somar = (x, y) => x + y
+// console.log(somar(10, 5))
+
+// function sobrenome(sob) {
+//     return 'Ederson' + sob;
+// }
+
+// console.log(sobrenome('Samuel'))
+
+const sobrenome = (sob) => 'Ederson ' + sob
+
+console.log(sobrenome('Samuel'))
