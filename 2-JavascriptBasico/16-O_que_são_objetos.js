d6d8a09cfffde6fbec4f89c9ec68021ef1cf3ea2
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-13 18:13:18 -0300

    16 - O que são objetos

---
+++ 2-JavascriptBasico/16-O_que_são_objetos.js
@@ -0,0 +1,15 @@
+let personagem = {
+    nome: 'Ederson',
+    idade: 90,
+    pais: 'Brasil',
+    olhos: ['preto', 'azul'],
+    caracteristicas: {
+        forca: 20,
+        magia: 5,
+        stamina: 15
+    }
+}
+
+console.log(`${personagem.nome} tem ${personagem.idade} anos.`)
+console.log()
+console.log(personagem.olhos)
