dbfee3f3eccb8e9ece717a3a47af4a067470e032
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-13 17:47:05 -0300

    15-Exercício_array

--- 2-JavascriptBasico/13-Introdução_Array.js
+++ 2-JavascriptBasico/13-Introdução_Array.js
@@ -1,3 +1,21 @@
 let colors = ['blue', 'red', 'green'];
+let idades = [10, 20, 30]
+
+let lista0 = ['algo', 30, 'info', true]
+
+let lista2 = ['info', nomes]
+
+let lista3 = ['info', ['xx', 'yy']]
+
+let segundo = lista[1]
+
+let lista = [
+    'info',
+    ['xx', 'yy']
+];
+
+console.log(segundo[0])
 
 console.log(colors[1])
+
+console.log(lista[0])
---
+++ 2-JavascriptBasico/14-Operações_básicas_array.js
@@ -0,0 +1,12 @@
+let ingredientes = [
+    'agua',
+    'farinha',
+    'ovo',
+    'água'
+];
+
+// pop = remove o último
+// shift = remove o primeiro
+
+console.log(ingredientes)
+console.log(`Total de ingredientes: ${ingredientes.length}`);
