a1b7163cbb64b461efdace793fb9104411e8310d
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-14 16:35:19 -0300

    Finalizando Javascript Básico

--- 2-JavascriptBasico/25-Iteração_de_array1.js
+++ 2-JavascriptBasico/25-Iteração_de_array1.js
@@ -1,11 +1,7 @@
 let fruits = ['Banana', 'Laranja', 'Maça', 'Pêra', 'Uva']
 
-let ok = fruits.every((value) => {
-    return value.length > 3
-})
-
-if (ok) {
-    console.log('Todos são maior que 3')
+if(fruits.includes('Uva')) {
+    console.log('tem uva sim!')
 } else {
-    console.log('Não são todos maior que 3')
+    console.log('Não tem uva...')
 }
