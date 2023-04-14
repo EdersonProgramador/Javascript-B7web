2be085a86d9368f60d6aea4c0b41ee2367ecc89a
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-14 12:52:48 -0300

    24 - Ordenação de array

---
+++ 2-JavascriptBasico/24-Ordenação_de_array.js
@@ -0,0 +1,8 @@
+let cars = [
+    { brand: 'Fiat', year: 2022 },
+    { brand: 'Bmw', year: 2018 },
+    { brand: 'Ferrari', year: 2020 }
+]
+
+cars.sort((a, b) => b.year - a.year);
+console.log(cars)
