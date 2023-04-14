7f73fa30b7df6a5f7a747b3373fac547d4ac395b
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-14 11:56:16 -0300

    20-Dando loop em arrays

---
+++ 2-JavascriptBasico/20-Dando_loop_em_arrays.js
@@ -0,0 +1,19 @@
+let cores = [
+    {nome: 'preto', qt: 10},
+    {nome: 'azul', qt: 5},
+    {nome: 'vermelho', qt: 15}
+]
+
+
+
+// for (let n = 0; n < cores.length; n++) {
+//     console.log(cores[n])
+// }
+
+// for(let i in cores) {
+//     console.log(cores[i])
+// }
+
+for (let cor of cores) {
+    console.log(`Nome: ${cor.nome} - ${cor.qt}`);
+}
