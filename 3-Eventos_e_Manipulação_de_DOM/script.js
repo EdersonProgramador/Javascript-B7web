941cfc0967171cc360c3778d1edf7fa1b94c63ce
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-17 11:31:21 -0300

    Treinando JS

--- 3-Eventos_e_Manipulação_de_DOM/script.js
+++ 3-Eventos_e_Manipulação_de_DOM/script.js
@@ -1,12 +1,6 @@
-function clicou() {
-    const input = document.querySelector('input');
-    const botao = document.querySelector('.botao')
-
-    if (input.getAttribute('type') === 'text') {
-        input.setAttribute('type', 'password')
-        botao.innerText = "Mostrar Senha";
-    } else {
-        input.setAttribute('type', 'text')
-        botao.innerText = "Ocultar Senha";
-    }
+function soltou(e) {
+    console.log(e.code);
 }
+
+const input = document.querySelector('input');
+input.addEventListener('keyup', soltou);
